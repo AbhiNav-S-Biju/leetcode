ef05a4a9d33f66d82676defa8e30fdec66cc62ff
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 02:13:34 +0530

    Add LeetCode Medium - Evaluate Reverse Polish Notation

---
+++ leetcode/Medium/0150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -0,0 +1,36 @@
+// LeetCode Problem: Evaluate Reverse Polish Notation
+// Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+// Difficulty: Medium
+// Language: java
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for(String s:tokens){
+            if(s.equals("+")){
+                int num1 = st.pop();
+                int num2 = st.pop();
+                st.push(num1+num2);
+            }
+            else if(s.equals("-")){
+                int num1 = st.pop();
+                int num2 = st.pop();
+                st.push(num2-num1);
+            }
+            else if(s.equals("*")){
+                int num1 = st.pop();
+                int num2 = st.pop();
+                st.push(num1*num2);
+            }
+            else if(s.equals("/")){
+                int num1 = st.pop();
+                int num2 = st.pop();
+                st.push((int)num2/num1);
+            }
+            else{
+                st.push(Integer.parseInt(s));
+            }
+        }
+        return st.pop();
+    }
+}
