b614e44f4e924c940bbb0db1f72d15121d4fa8ff
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 02:14:02 +0530

    Add LeetCode Medium - Sort Colors

---
+++ leetcode/Medium/0075-Sort-Colors/solution.java
@@ -0,0 +1,28 @@
+// LeetCode Problem: Sort Colors
+// Link: https://leetcode.com/problems/sort-colors/
+// Difficulty: Medium
+// Language: java
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        int i = 0;
+        while(i<=high){
+            if(nums[i] == 0){
+                int temp = nums[i];
+                nums[i] = nums[low];
+                nums[low] = temp;
+                i++;
+                low++;
+            }else if(nums[i] == 2){
+                int temp = nums[i];
+                nums[i] = nums[high];
+                nums[high] = temp;
+                high--;
+            }else{
+                i++;
+            }
+        }
+    }
+}
