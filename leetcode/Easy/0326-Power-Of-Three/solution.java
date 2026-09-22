bd0e64d9e54955bf0fc056af8e354f991060b011
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 02:16:34 +0530

    Add LeetCode Easy - Power of Three

---
+++ leetcode/Easy/0326-Power-Of-Three/solution.java
@@ -0,0 +1,19 @@
+// LeetCode Problem: Power of Three
+// Link: https://leetcode.com/problems/power-of-three/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+         if (n <= 0) {
+            return false;
+        }
+        while(n%3 == 0){
+            n = n/3;
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+}
