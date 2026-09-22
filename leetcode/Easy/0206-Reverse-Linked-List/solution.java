dbbdc174c4ed65105d732b2f065b2f5f3e923831
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 02:13:56 +0530

    Add LeetCode Easy - Reverse Linked List

---
+++ leetcode/Easy/0206-Reverse-Linked-List/solution.java
@@ -0,0 +1,19 @@
+// LeetCode Problem: Reverse Linked List
+// Link: https://leetcode.com/problems/reverse-linked-list/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
