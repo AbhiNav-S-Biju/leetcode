b18e020a4857ea74d60a7ecc494d0267eed1aa99
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 02:12:42 +0530

    Add LeetCode Easy - Minimum Depth of Binary Tree

---
+++ leetcode/Easy/0111-Minimum-Depth-Of-Binary-Tree/solution.java
@@ -0,0 +1,37 @@
+// LeetCode Problem: Minimum Depth of Binary Tree
+// Link: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+// Difficulty: Easy
+// Language: java
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(root.left == null){
+            return 1+right;
+        }
+        if(root.right == null){
+            return 1+left;
+        }
+        return  1+ Math.min(left, right);
+
+    }
+}
