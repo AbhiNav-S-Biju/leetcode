debe775bb4e64a6da9dceecc1ce3eac29d4af794
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 02:16:22 +0530

    Add LeetCode Easy - Plus One

---
+++ leetcode/Easy/0066-Plus-One/solution.java
@@ -0,0 +1,20 @@
+// LeetCode Problem: Plus One
+// Link: https://leetcode.com/problems/plus-one/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i = n-1; i>=0; i--){
+            if(digits[i]!=9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] result = new int[n+1];
+        result[0] = 1;
+        return result;
+    }
+}
